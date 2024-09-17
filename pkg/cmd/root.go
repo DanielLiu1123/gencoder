43b0250050a2f9b74483e6314d5280a8926bd09f
Freeman Liu <llw599502537@gmail.com>
2024-09-17 16:21:41 +0800

    Support to print version info

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -7,18 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func NewCmdRoot() *cobra.Command {
+func NewCmdRoot(buildInfo *model.BuildInfo) *cobra.Command {
 
 	opt := &model.GlobalOptions{}
 
 	c := &cobra.Command{
-		Use:   "gencoder <command> [flags]",
-		Short: "The ultimate code generator",
-		Long: `
-gencoder is a code generator that generates code from templates/databases, for any language or framework.
+		Use:     "gencoder",
+		Version: buildInfo.Version,
+		Short:   "The ultimate code generator",
+		Long:    "gencoder is a code generator that generates code from templates/databases, for any languages/frameworks.",
+		Example: `  # Generate code from default config file (gencoder.yaml)
+  $ gencoder generate
 
-$ gencoder generate -f gencoder.yaml
-$ gencoder introspect -f gencoder.yaml`,
+  # Generate code from a specific config file
+  $ gencoder generate -f myconfig.yaml`,
 	}
 
 	c.Flags().StringVarP(&opt.Config, "config", "f", "gencoder.yaml", "Config file to use")
--- pkg/model/options.go
+++ pkg/model/options.go
@@ -1,5 +1,12 @@
 package model
 
+import "time"
+
 type GlobalOptions struct {
 	Config string `json:"config" yaml:"config"` // Where to read gencoder.yaml
+}
+
+type BuildInfo struct {
+	Version   string
+	BuildTime time.Time
 }
