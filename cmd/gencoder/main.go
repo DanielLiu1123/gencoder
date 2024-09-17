b9d788fa7a7a6080586039a27dcaa884b05ea1a3
Freeman Liu <llw599502537@gmail.com>
2024-09-17 16:24:22 +0800

    Support to print version info (#2)

--- cmd/gencoder/main.go
+++ cmd/gencoder/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/DanielLiu1123/gencoder/pkg/cmd"
+	"github.com/DanielLiu1123/gencoder/pkg/model"
 	"log"
+	"time"
 
 	// drivers
 	_ "github.com/go-sql-driver/mysql"
@@ -12,11 +14,18 @@ import (
 	_ "github.com/sijms/go-ora/v2"
 )
 
+var version = "0.0.1"
+
 func main() {
 
 	log.SetFlags(0)
 
-	err := cmd.NewCmdRoot().Execute()
+	buildInfo := &model.BuildInfo{
+		Version:   version,
+		BuildTime: time.Now(),
+	}
+
+	err := cmd.NewCmdRoot(buildInfo).Execute()
 	if err != nil {
 		log.Fatal(err)
 	}
